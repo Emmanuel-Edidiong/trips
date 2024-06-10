6b02db34c40c39d3edc84166032f623145781afb
emmanueledidiong <akpanemmanueledidiong99@gmail.com>
2024-06-10 16:55:45 +0100

    new changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,12 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off',  // example of ignoring a specific rule
-    '@typescript-eslint/explicit-module-boundary-types': 'off',  // another example
-    // add other rules you want to ignore
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-namespace': 'off',
   }
 }
